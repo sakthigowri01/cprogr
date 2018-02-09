8b6c4b2baad9f428f374af4cbfb63f21ef760ebd
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-02-09 16:28:56 +0530

    w.c

---
+++ w.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int lowerLimit, upperLimit;
+    
+    printf("Enter lower limit: ");
+    scanf("%d", &lowerLimit);
+    printf("Enter upper limit: ");
+    scanf("%d", &upperLimit);
+    
+    printf("Even/odd Numbers from %d to %d are: ", lowerLimit, upperLimit);
+    printEvenOdd(lowerLimit, upperLimit); 
+    
+    return 0;
+}
+
+void printEvenOdd(int cur, int limit)
+{
+    if(cur > limit)
+        return;
+    
+    printf("%d, ", cur);
+    
+ getch();
+}
